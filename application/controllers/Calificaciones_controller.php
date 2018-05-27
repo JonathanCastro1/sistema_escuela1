4edb569dc73cfc507ce3779a1270e9edcfe40a32
JonathanCastro1 <38263950+JonathanCastro1@users.noreply.github.com>
2018-05-27 15:50:58 -0400

    Update Calificaciones_controller.php

--- application/controllers/Calificaciones_controller.php
+++ application/controllers/Calificaciones_controller.php
@@ -6,9 +6,9 @@ class Calificaciones_controller extends CI_Controller {
 	public function index()
 	{
 		$data['usuario'] = $this->session->userdata('usuario');
-		$data['contrasena'] = $this->session->userdata('contrasena');
+		// $data['contrasena'] = $this->session->userdata('contrasena');
 
-		// $data['datos'] = $this->usuarios_model->seleccionarUsuarios();
+		// $data['datos'] = $this->calificaciones_model->seleccionar();
 
 
 
@@ -17,6 +17,42 @@ class Calificaciones_controller extends CI_Controller {
 		$this->load->view('include/sidebar');
 		$this->load->view('admin/calificaciones',$data);
 		$this->load->view('include/footer');
+	}
+
+	 public function agregarCalificaciones()	{                 
+                   
+				
+			if (isset($_POST['submit'])) {	
+
+		    $alumno = $this->input->post('alumno');
+			$nota = $this->input->post('nota');			
+			$descripcion = $this->input->post('descripcion');
+			$fecha = $this->input->post('fecha');
+			$session = $this->input->post('session');
+			$turno = $this->input->post('turno');
+			$sede = $this->input->post('sede');			
+
+			$this->calificaciones_model->agregarCalificaciones($alumno ,$nota ,$descripcion , $fecha,$session ,$turno ,$sede);
+
+			redirect(base_url("index.php/calificaciones_controller"));
+					
+                         	
+            } else {			
+
+			redirect(base_url("index.php/dashboard_controller"));
+		}		
+
+	}
+
+
+
+		public function estudiantesAprobados()
+	{
+		
+		$data['datos'] = $this->calificaciones_model->estudiantesAprobados();
+		
+		$this->load->view('admin/dashboard',$data);
+		
 	}
 
 
