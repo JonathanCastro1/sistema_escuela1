89d91a12b2edfa237a0ec77fecead9ddae82cb6d
JonathanCastro1 <38263950+JonathanCastro1@users.noreply.github.com>
2018-05-27 15:52:47 -0400

    Update Usuarios_model.php

--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -34,6 +34,45 @@ class Usuarios_model extends CI_Model {
 	}
 
 
+		public function editarUsuarios($nombre,$apellido ,$nacimiento ,$email ,$telefono , $id)
+	{			
+				
+		$sql = "UPDATE usuarios SET nombre = '$nombre',
+				apellido = '$apellido',
+				nacimiento = '$nacimiento',
+				email = '$email',
+				telefono = $telefono
+		       WHERE id = $id";
+
+		$query = $this->db->query($sql);    	
+		
+		 return $query;
+	}
+
+
+	public function usuariosPorId($id)
+	{
+		
+		$sql = "SELECT id,nombre,apellido,nacimiento,email,telefono from usuarios where id= $id";
+
+		$query = $this->db->query($sql);
+
+		return $query->row();
+	}
+
+
+	public function eliminarUsuarios($id)
+	{			
+				
+		$sql = "DELETE from usuarios where id='$id'";
+
+		$query = $this->db->query($sql);    	
+		
+		 return $query;
+	}
+
+
+
 		public function totalUsuarios()
 	{		
 		
