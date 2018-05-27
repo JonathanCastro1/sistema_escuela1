1407b1477772a211c1c606c81be85542ecc2aa26
JonathanCastro1 <38263950+JonathanCastro1@users.noreply.github.com>
2018-05-27 15:53:45 -0400

    Update Profesores_model.php

--- application/models/Profesores_model.php
+++ application/models/Profesores_model.php
@@ -32,6 +32,46 @@ class Profesores_model extends CI_Model {
 		return $query;
 	}
 
+
+	public function editarProfesores($nombre ,$apellido ,$email ,$session ,$turno ,$sede ,$id)
+	{			
+				
+		$sql = "UPDATE profesores SET nombre = '$nombre',
+				apellido = '$apellido',				
+				email = '$email',
+				session = '$session',
+				turno = '$turno',
+				sede = '$sede'
+		       WHERE id = $id";
+
+		$query = $this->db->query($sql);    	
+		
+		 return $query;
+	}
+
+
+	public function profesoresPorId($id)
+	{
+		
+		$sql = "SELECT id,nombre,apellido,email,session,turno,sede from profesores where id= $id";
+
+		$query = $this->db->query($sql);
+
+		return $query->row();
+	}
+
+
+		public function eliminarProfesores($id)
+	{			
+				
+		$sql = "DELETE from profesores where id='$id'";
+
+		$query = $this->db->query($sql);    	
+		
+		 return $query;
+	}
+
+
 		public function totalProfesores()
 	{		
 		
